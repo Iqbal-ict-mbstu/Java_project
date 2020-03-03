89b57f2e55fc346da81597131aad2104390873d6
Iqbal-ict-mbstu <iqbalhossen1563@gmail.com>
2020-03-03 21:21:48 +0600

    Iqbal

---
+++ src/emailsending/Session.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emailsending;
+
+/**
+ *
+ * @author Dell
+ */
+class Session {
+    
+}
